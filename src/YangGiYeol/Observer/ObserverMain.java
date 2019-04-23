bb7c5afc2bd0f4c7b5f6fbaa55677d6069a26d1f
yky <ykycome00@gmail.com>
2019-04-23 17:21:22 +0900

    옵저버 커밋

--- src/YangGiYeol/Observer/ObserverMain.java
+++ src/YangGiYeol/Observer/ObserverMain.java
@@ -1,6 +1,6 @@
 package YangGiYeol.Observer;
 
-//동작 테스트용 클래스
+//동작 테스트용 클래스7878
 public class ObserverMain {
 	public static void main(String[] args) {
 		
@@ -8,10 +8,11 @@ public class ObserverMain {
 		Observer observer1 = new DigitObserver();
 		Observer observer2 = new GraphObserver();
 		
+		int a = 0;
+		
 		generator.addObserver(observer1);
 		generator.addObserver(observer2);
 		generator.execute();
-		
 		
 	}
 }
